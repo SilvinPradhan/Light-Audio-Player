01e0edb10be90517e4f8972287fc5948d2d6a1b3
Silvin Pradhan <silvinpradhan95@gmail.com>
2020-10-21 16:23:26 -0500

    reinitialised the repo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,112 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'AudioPlayer';
+  audioObj = new Audio();
+  audioEvents = [
+    'ended',
+    'error',
+    'play',
+    'playing',
+    'pause',
+    'timeupdate',
+    'canplay',
+    'loadedmetadata',
+    'loadstart',
+  ];
+  files = [
+    {
+      url:
+        'http://conferencenow.info/audio/TiengNoiQueHuong/200917-Pho%cc%89ngV%c3%a2%cc%81nMSKi%c3%aa%cc%81n.mp3',
+      name: 'sample',
+    },
+
+    { url: './assets/sound1.mp3', name: 'Test 2 Song' },
+
+    { url: './assets/sound3.mp3', name: 'Test 3 Song' },
+
+    { url: './assets/sound3.mp3', name: 'Test 4 Song' },
+
+    { url: './assets/sound3.mp3', name: 'Test 5 Song' },
+
+    { url: './assets/sound3.mp3', name: 'Test 6 Song' },
+  ];
+
+  currentTime = '00:00:00';
+  duration = '00:00:00';
+  seek = 0;
+
+  streamObserver(url) {
+    return new Observable((observer) => {
+      this.audioObj.src = url;
+      this.audioObj.load();
+      this.audioObj.play();
+
+      const handler = (event: Event) => {
+        console.log(event);
+        this.seek = this.audioObj.currentTime;
+        this.duration = this.timeFormat(this.audioObj.duration);
+        this.currentTime = this.timeFormat(this.audioObj.currentTime);
+      };
+
+      this.addEvent(this.audioObj, this.audioEvents, handler);
+
+      return () => {
+        this.audioObj.pause();
+        this.audioObj.currentTime = 0;
+
+        this.removeEvent(this.audioObj, this.audioEvents, handler);
+      };
+    });
+  }
+
+  addEvent(obj, events, handler) {
+    events.forEach((event) => {
+      obj.addEventListener(event, handler);
+    });
+  }
+
+  removeEvent(obj, events, handler) {
+    events.forEach((event) => {
+      obj.removeEventListener(event, handler);
+    });
+  }
+
+  setSeekTo(ev) {
+    this.audioObj.currentTime = ev.target.value;
+  }
+
+  setVolume(ev) {
+    this.audioObj.volume = ev.target.value;
+  }
+
+  openFile(url) {
+    this.streamObserver(url).subscribe((event) => {});
+
+    console.log(url);
+  }
+
+  play() {
+    this.audioObj.play();
+  }
+  pause() {
+    this.audioObj.pause();
+    console.log('Clicked Pause Button');
+  }
+  stop() {
+    this.audioObj.pause();
+    this.audioObj.currentTime = 0;
+    console.log('Clicked Stop button');
+  }
+
+  timeFormat(time, format = 'HH:mm:ss') {
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon'
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from '@angular/material/toolbar'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    IonicModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  ngOnInit() : void {
+
+  }
+}
